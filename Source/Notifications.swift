7ca4232c29578ed32b866dd5a71c5a1614415e62
曾文斌 <ywwzwb@gmail.com>
2017-09-29 22:45:11 +0800

    通知类好了

--- Source/Notifications.swift
+++ Source/Notifications.swift
@@ -23,19 +23,22 @@
 //
 
 import Foundation
-
+// 对 notifycation.name 的扩展 用于发送与接收通知
 extension Notification.Name {
+    // 使用了一个内部类型, 用于指示通知名, 在这里作用类似于一个命名空间
+    //使用方法为 NotificationCenter.default.post(name: NSNotification.Name.Task.DidCancel, object: nil)
     /// Used as a namespace for all `URLSessionTask` related notifications.
     public struct Task {
+        /// 在 sessionTask 继续运行的时候会发送通知, 通知的 object 中含有此 task
         /// Posted when a `URLSessionTask` is resumed. The notification `object` contains the resumed `URLSessionTask`.
         public static let DidResume = Notification.Name(rawValue: "org.alamofire.notification.name.task.didResume")
-
+        /// 在 sessionTask 停止的时候会发送通知, 通知的 object 中含有此 task
         /// Posted when a `URLSessionTask` is suspended. The notification `object` contains the suspended `URLSessionTask`.
         public static let DidSuspend = Notification.Name(rawValue: "org.alamofire.notification.name.task.didSuspend")
-
+        /// 在 sessionTask 取消的时候会发送通知, 通知的 object 中含有此 task
         /// Posted when a `URLSessionTask` is cancelled. The notification `object` contains the cancelled `URLSessionTask`.
         public static let DidCancel = Notification.Name(rawValue: "org.alamofire.notification.name.task.didCancel")
-
+        /// /// 在 sessionTask 结束的时候会发送通知, 通知的 object 中含有此 task
         /// Posted when a `URLSessionTask` is completed. The notification `object` contains the completed `URLSessionTask`.
         public static let DidComplete = Notification.Name(rawValue: "org.alamofire.notification.name.task.didComplete")
     }
@@ -44,8 +47,10 @@ extension Notification.Name {
 // MARK: -
 
 extension Notification {
+    /// 这里也是起到一个命名空间的作用, 用于标记指定键值
     /// Used as a namespace for all `Notification` user info dictionary keys.
     public struct Key {
+        /// 用于标记 userinfo 中 sessiontask 的键值
         /// User info dictionary key representing the `URLSessionTask` associated with the notification.
         public static let Task = "org.alamofire.notification.key.task"
     }
